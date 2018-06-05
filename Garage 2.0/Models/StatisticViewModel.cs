e5441407f8f8ee14cc328add3f21d5d94862fadd
tomas.risveden <tomas.risveden@gmail.com>
2018-06-05 14:47:47 +0200

    Redovisning

---
+++ Garage 2.0/Models/StatisticViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Garage_2._0.Models
+{
+    public class StatisticViewModel 
+    {
+        public StatisticViewModel statisticVieModel { get; set; }
+        public Dictionary<string,double> Dictionary { get; set; }
+    }
+}
